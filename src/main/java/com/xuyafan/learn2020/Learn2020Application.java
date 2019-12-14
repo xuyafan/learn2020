56a9c5cbcdb0abbd57eb59bae8b189ebf75d1a32
Softfun <xyf18362929560@163.com>
2019-12-14 12:47:56 +0800

    Initial commit

---
+++ src/main/java/com/xuyafan/learn2020/Learn2020Application.java
@@ -0,0 +1,13 @@
+package com.xuyafan.learn2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuyafan/learn2020/Learn2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuyafan.learn2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
